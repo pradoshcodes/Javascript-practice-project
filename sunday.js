ed8760575e8ae81322ff31209c89809cb1e4d808
pradoshcodes <pradoshmohanty034@gmail.com>
2024-04-04 08:21:07 +0530

    practice

---
+++ sunday.js
@@ -0,0 +1,11 @@
+//Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.
+
+console.log("--------------------------------------------------------------------");
+
+for (let year = 2014 ; year <= 2050 ; year++) {
+    let date = new Date(year,0,1);
+
+    if(date.getDay() === 0) {
+       console.log("1st January is being a Sunday  " + year);   
+    }
+}
