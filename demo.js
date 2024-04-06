f351440e8fa4019159103efa2cf328ad6b580c72
pradoshcodes <pradoshmohanty034@gmail.com>
2024-04-06 22:54:28 +0530

    calculator

---
+++ demo.js
@@ -0,0 +1,36 @@
+let size=[[0,1],[2,3]]
+let total=0
+for(array of  size){
+   //   for (i of array) {
+        total=total+array;
+     
+}
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+
+
+
+
+
+
+
---
+++ main project/calculator.js
@@ -0,0 +1,6 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let String = "";
+let arr = Array.from(button);
+arr.forEach()
